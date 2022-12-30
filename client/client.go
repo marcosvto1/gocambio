92fa6749273012b9d1c1e3b38bf820a0bddaf5b0
Marcos Oliveira <marcos.oliveira.ext@infracommerce.com.br>
2022-12-30 14:12:27 -0300

    feat: remove attributes struct unsed

--- client/client.go
+++ client/client.go
@@ -11,17 +11,7 @@ import (
 )
 
 type CotacaoValue struct {
-	Code string `json:"code"`
-	CodeIn string `json:"codein"`
-	Name string `json:"name"`
-	High string `json:"high"`
-	Low string `json:"low"`
-	VarBid string `json:"varBid"`
-	PctChange string `json:"pctChange"`
 	Bid string `json:"bid"`
-	Ask string `json:"Ask"`
-	Timestamp string `json:"timestamp"`
-	CreateDate string `json:"create_date"`
 }
 
 type Cotacao struct {
