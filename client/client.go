30f12280d49c4daaa59129d41e7ba633360ba7a9
Marcos Oliveira <marcos.oliveira.ext@infracommerce.com.br>
2022-12-31 13:08:13 -0300

    feat: handle context error

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -18,6 +19,10 @@ type Cotacao struct {
 	USDBRL CotacaoValue
 }
 
+type ErrorMessage struct {
+	Message string `json:"message"`
+}
+
 func main() {
 	ctx := context.Background()
 	ctx, cancel := context.WithTimeout(ctx, time.Millisecond * 300)
@@ -25,29 +30,35 @@ func main() {
 
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8081/cotacao", nil)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer res.Body.Close()
-
+	
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+
+	if res.StatusCode != 200 {
+		var errorMessage ErrorMessage
+		json.Unmarshal(body, &errorMessage)
+		log.Fatal(errorMessage.Message)
 	}
 
 	arquivo, err := os.OpenFile("cotacao.txt", os.O_WRONLY, os.ModePerm)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	var cotacao Cotacao
 	err = json.Unmarshal(body, &cotacao)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	fmt.Printf("Dólar: %v \n", cotacao.USDBRL.Bid)
@@ -55,6 +66,6 @@ func main() {
 	_, err = arquivo.WriteString("Dólar: "+ cotacao.USDBRL.Bid)
 	arquivo.Close()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
